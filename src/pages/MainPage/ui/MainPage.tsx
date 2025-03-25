98d6e0c40b215dfb3a070d00e85e0f32d618d9a7
MintuevMR <mintuev.magomed@gmail.com>
2025-03-25 12:13:01 +0300

    fix storybook

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,3 @@
-import { Counter } from 'entities/Counter';
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 const MainPage = () => {
@@ -7,7 +5,6 @@ const MainPage = () => {
 
     return (
         <div>
-            <Counter />
             {t('Главная страница')}
         </div>
     );
